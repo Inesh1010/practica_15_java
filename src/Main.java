3513d1cb82abf6a6af667f5a864fadedac80dc41
kek <kek@lord.com>
2020-12-19 11:55:19 +0300

    vvvv

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class Main {
 
     public static <E> List<E> asList(E[] a) {
-        return new OwnArray<E>(a);
+        return new arr<E>(a);
     }
 
-    public static class OwnArray<E> extends AbstractList<E> {
+    public static class arr<E> extends AbstractList<E> {
 
         private final E[] a;
 
-        OwnArray(E[] array) {
+        arr(E[] array) {
             a = array;
         }
 
@@ -34,42 +34,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Student> studentsArList = new ArrayList<>();
-        LinkedList<Student> studentsLinkList = new LinkedList<>();
+        ArrayList<Student> studAr = new ArrayList<>();
+        LinkedList<Student> studLink = new LinkedList<>();
 
         for (int i = 0; i < 10000000; ++i) {
-            studentsArList.add(new Student(33));
-            studentsLinkList.add(new Student(45));
+            studAr.add(new Student(33));
+            studLink.add(new Student(45));
         }
 
-
         long t0 = System.currentTimeMillis();
-        Student student = studentsArList.get(8987426);
+        Student stud = studAr.get(7945332);
         long t1 = System.currentTimeMillis();
 
         long timeArList = t1 - t0;
 
-
         t0 = System.currentTimeMillis();
-        student = studentsLinkList.get(8987426);
+        stud = studLink.get(7945332);
         t1 = System.currentTimeMillis();
 
         long timeLinkList = t1 - t0;
 
-        System.out.println("ArrayList: " + timeArList);
-        System.out.println("LinkedList: " + timeLinkList);
+        System.out.println("ArList: " + timeArList);
+        System.out.println("LinkList: " + timeLinkList);
 
 
-        student = new Student(43);
+        stud = new Student(23);
 
         t0 = System.currentTimeMillis();
-        studentsArList.add(77657, student);
+        studAr.add(84532, stud);
         t1 = System.currentTimeMillis();
 
         timeArList = t1 - t0;
 
         t0 = System.currentTimeMillis();
-        studentsLinkList.add(77657, student);
+        studLink.add(84532, stud);
         t1 = System.currentTimeMillis();
 
         timeLinkList = t1 - t0;
@@ -81,20 +79,16 @@ public class Main {
         Student[] array = new Student[10000000];
 
         for (int i = 0; i < 10000000; ++i) {
-            array[i] = new Student(i+6);
+            array[i] = new Student(i+2);
         }
 
-        OwnArray<Student> studentsOA = new OwnArray<>(array);
+        arr<Student> studOA = new arr<>(array);
 
-        student = studentsOA.get(8987426);
-
-        System.out.println("студент 8987426: " + student);
-
-        studentsOA.set(8987426, new Student(-23));
-
-        student = studentsOA.get(8987426);
-
-        System.out.println("студент 8987426: " + student);
+        stud = studOA.get(7945332);
+        System.out.println("студент 8987426: " + stud);
+        studOA.set(7945332, new Student(-23));
+        stud = studOA.get(7945332);
+        System.out.println("студент 8987426: " + stud);
 
     }
 
